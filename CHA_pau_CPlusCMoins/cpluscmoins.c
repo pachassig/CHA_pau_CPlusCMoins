15767ed71430751d289935b675c199bba7ad1be1
pachassig <pchassignol@gamingcampus.in>
2025-10-29 15:28:11 +0100

    add borne + nombre d'essais

--- CHA_pau_CPlusCMoins/cpluscmoins.c
+++ CHA_pau_CPlusCMoins/cpluscmoins.c
@@ -3,12 +3,17 @@
 #include <stdlib.h>
 
 int main() {
-	int a_trouver = rand() % 100 + 1;
+	int borne = 0;
+	printf("Jusqu'a quel nombre veux-tu que je choisisse ?");
+	scanf("%d", &borne);
+	int a_trouver = rand() % borne + 1;
 	int nombre_choisi = -1;
+	int nombre_essais = 0;
 	printf("J'ai choisi un nombre. Essaie de le trouver !\n");
 	while (a_trouver != nombre_choisi) {
 		printf("Choisis un nombre : ");
 		scanf("%d", &nombre_choisi);
+		nombre_essais += 1;
 		if (a_trouver < nombre_choisi) {
 			printf("C'est moins ! \n");
 		}
@@ -19,7 +24,8 @@ int main() {
 			break;
 		}
 	}
-	printf("Bravo ! Mon nombre etait bien %d !",a_trouver);
+	printf("Bravo ! Mon nombre etait bien %d !\n",a_trouver);
+	printf("Tu l'as trouvé en %d essais !",nombre_essais);
 	return 0;
 }
 
