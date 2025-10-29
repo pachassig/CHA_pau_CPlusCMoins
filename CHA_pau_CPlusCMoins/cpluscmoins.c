f237123909108a700a5337b9da45742771f73482
pachassig <pchassignol@gamingcampus.in>
2025-10-29 16:14:40 +0100

    add nombre d'essais max

--- CHA_pau_CPlusCMoins/cpluscmoins.c
+++ CHA_pau_CPlusCMoins/cpluscmoins.c
@@ -1,19 +1,32 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main() {
+	char limite;
+	printf("Voulez-vous un nombre d'essais limite ? (O / N)\n");
+	scanf("%c", &limite);
+	printf("%c\n", limite);
+	int nombre_essais = 1;
+	if (limite == 'O' || limite == 'o') {
+		printf("Combien d'essais voulez-vous ?");
+		scanf("%d", &nombre_essais);
+	}
 	int borne = 0;
 	printf("Jusqu'a quel nombre veux-tu que je choisisse ?");
 	scanf("%d", &borne);
 	int a_trouver = rand() % borne + 1;
 	int nombre_choisi = -1;
-	int nombre_essais = 0;
 	printf("J'ai choisi un nombre. Essaie de le trouver !\n");
-	while (a_trouver != nombre_choisi) {
+	int essais = 0;
+	while (a_trouver != nombre_choisi && nombre_essais > 0) {
 		printf("Choisis un nombre : ");
 		scanf("%d", &nombre_choisi);
-		nombre_essais += 1;
+		if (limite == 'O' || limite == 'o') {
+			nombre_essais -= 1;
+		}
+		essais += 1;
 		if (a_trouver < nombre_choisi) {
 			printf("C'est moins ! \n");
 		}
@@ -24,8 +37,13 @@ int main() {
 			break;
 		}
 	}
-	printf("Bravo ! Mon nombre etait bien %d !\n",a_trouver);
-	printf("Tu l'as trouvé en %d essais !",nombre_essais);
+	if (nombre_essais == 0) {
+		printf("Dommage ! Mon nombre etait %d !\n", a_trouver);
+	}
+	else {
+		printf("Bravo ! Mon nombre etait bien %d !\n", a_trouver);
+		printf("Tu l'as trouvé en %d essais !", essais);
+	}
 	return 0;
 }
 
